3b8fce59ffea63f1a10232a3162b2d094cd43946
Lukasz Ludwin <gorli@interia.pl>
2017-06-27 12:30:17 +0100

    second commit

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const b = 'makota';
 const c = `${a}${b}`;
 console.log(c);
 //2
-const multiply = (x=1,y=1) => x*y;
-console.log(multiply(2,3));
+const multiply = (x = 1, y = 1) => x * y;
+console.log(multiply(2, 3));
 console.log(multiply(3));
 //3
 const average = (...args) => {
-	let sum =0;
-	for (let i=0; i<args.length;i++) {
+	let sum = 0;
+	for (let i = 0; i < args.length; i++) {
 		sum += args[i];
 	}
-	return sum/args.length;
+	return sum / args.length;
 }
-console.log(average(1,2,3,4));
+console.log(average(1, 2, 3, 4));
 //4
-const sd = [1,2,3,4,5];
+const sd = [1, 2, 3, 4, 5];
 console.log(average(...sd));
 //5
-const table = [1,4,'iwona', false, 'nowak']; 
-const [,,firstName,,lastName] = table;
-console.log(firstName,lastName); 
+const table = [1, 4, 'iwona', false, 'nowak']; 
+const [, , firstName, , lastName] = table;
+console.log(firstName, lastName); 
